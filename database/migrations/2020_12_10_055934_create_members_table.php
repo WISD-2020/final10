d3979f40951e8d1511ea1949bb9041c936653836
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-12-10 14:01:02 +0800

    Member ：model,controller,seeder,migration,factory建立 以及Member migration 資料表欄位設定

---
+++ database/migrations/2020_12_10_055934_create_members_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedbigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('sex');
+            $table->string('email');
+            $table->string('address');
+            $table->string('tel');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
