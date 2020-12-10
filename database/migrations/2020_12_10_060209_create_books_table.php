6c4e6e916bbe60db14220082584c9166238776b0
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-12-10 14:04:38 +0800

    Book：model,controller,seeder,migration,factory建立 以及Book migration 資料表欄位設定

---
+++ database/migrations/2020_12_10_060209_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('member_id');
+            $table->foreign('member_id')->references('id')->on('members')->onDelete('cascade');
+            $table->string('name');
+            $table->string('quantity');
+            $table->integer('price');
+            $table->string('path');
+            $table->string('info');
+            $table->string('category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
