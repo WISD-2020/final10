08c596ee48d13d456ccba115b6aa4139cd994eed
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-12-14 16:04:40 +0800

    樣板套用、HomeController建立、首頁建立

---
+++ resources/views/home.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+
+<head>
+
+    <meta charset="utf-8">
+
+
+    <title>二手書購物網站</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="{{ asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="{{asset('fonts/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="{{asset('fonts/simple-line-icons/css/simple-line-icons.css')}}" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="{{ asset('css/stylish-portfolio.min.css')}}" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+<!-- Navigation -->
+<a class="menu-toggle rounded" href="#">
+    <i class="fas fa-bars"></i>
+</a>
+<nav id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+        <li class="sidebar-brand">
+            <a href="/home">二手書購物網站</a>
+        </li>
+        <li class="sidebar-nav-item">
+            <a href="/home">回首頁</a>
+        </li>
+        <li class="sidebar-nav-item">
+            <a href="#">賣場頁面</a>
+        </li>
+        <li class="sidebar-nav-item">
+            <a href="#"></a>
+        </li>
+        <li class="sidebar-nav-item">
+            <a href="#"></a>
+        </li>
+        <li class="sidebar-nav-item">
+            <a href="#"></a>
+        </li>
+    </ul>
+</nav>
+
+<!-- Header -->
+<header class="masthead d-flex">
+    <div class="container text-center my-auto">
+        <h1 class="mb-1">二手書購物網站</h1>
+        <h3 class="mb-5">
+        </h3>
+        <a class="btn btn-primary btn-lg" href="login" style="margin-right:100px;">登入</a>
+        <a class="btn btn-danger btn-lg" href="register">註冊</a>
+    </div>
+    <div class="overlay"></div>
+</header>
+
+
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded js-scroll-trigger" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Bootstrap core JavaScript -->
+<script src="{{ asset('jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+<!-- Plugin JavaScript -->
+<script src="{{ asset('jquery/jquery.easing.min.js')}}"></script>
+
+<!-- Custom scripts for this template -->
+<script src="js/stylish-portfolio.min.js"></script>
+
+</body>
+
+</html>
