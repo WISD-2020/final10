2f2a55a4b0695c2c7afa41e9d9fe8aca382b4fe1
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-12-17 16:59:52 +0800

    顯示訂單(購買)列表

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -12,9 +12,13 @@ class OrderController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $orders = Order::where('buyer_id', $request->user()->id)->get();
+        return view('orders.index', [
+            'orders' => $orders,
+        ]);
+
     }
 
     /**
---
+++ resources/views/orders/index.blade.php
@@ -0,0 +1,51 @@
+
+@extends('layouts.OrdersMaster')
+
+@section('content')
+
+    <!-- 訂單紀錄(購買) -->
+    @if (count($orders) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                訂單紀錄(購買)
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+
+                    <!-- 表頭 -->
+                    <thead>
+                    <th>訂單</th>
+                    <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- 表身 -->
+                    <tbody>
+                    @foreach ($orders as $order)
+                        <tr>
+                            <!-- 任務名稱 -->
+                            <td class="table-text">
+                                <div>賣家：{{ $order->seller_id }}</div>
+                                <div>買家：{{ $order->buyer_id }}</div>
+                                <div>書名：{{ $order->name }}</div>
+
+                            </td>
+
+                            <!-- 刪除按鈕 -->
+                            <td>
+                                <form action="/orders/{{ $order->id }}" method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+
+                                    <button>刪除訂單</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
+
+@endsection
