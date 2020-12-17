886e31d75b6195b13e895c6e0c15425d67d18e46
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-12-17 16:11:07 +0800

    shops.index更新

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+
+        return view('shops.index');
+    }
 }
