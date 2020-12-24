e9071e0458c0705e3183287c20e18e3b1bde9889
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-12-24 19:19:41 +0800

    更新：登入前首頁超連結 更新：books.create錯誤提示重複 更新：shopbar 連接

--- resources/views/layouts/shopbar.blade.php
+++ resources/views/layouts/shopbar.blade.php
@@ -2,7 +2,7 @@
 <link href="{{ asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet"><!--css樣式-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="#">二手書購物網站</a>
+        <a class="navbar-brand" href="/shops">二手書購物網站</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
