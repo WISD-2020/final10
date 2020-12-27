451be056d82e1fb776bf3b01dad97a43e784e3f2
3A732080 <tiffany89031176@gmail.com>
2020-12-28 01:46:40 +0800

    設定users資料表type欄位的預設值

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('sex');
             $table->string('email')->unique();
-            $table->string('type');
+            $table->string('type')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('address');
