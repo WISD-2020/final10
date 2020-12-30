2131a2d0d11fdaf2a17ab961107550cf176f1e6a
Chang Chen Feng <3a732100@gm.student.ncut.edu.tw>
2020-12-30 23:42:54 +0800

    更新：books.show版面

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -4,42 +4,62 @@
 </div>
 <title>商品資料</title>
 <style>
-    .uper {
+    .left {
         margin-top: 100px;
         margin-bottom:50px ;
         margin-left: 100px;
-        margin-right: 100px;
+        margin-right: 50%;
+    }
+    .right {
+
+        margin-left: 100px;
+        margin-right: 50px;
+    }
+    .width{
+        align-items: center;
     }
 </style>
-<div class="card uper">
-    <form action="/orders" method="POST" role="form" enctype="multipart/form-data">
-        @csrf
-        @method('POST')
+<div class="row left">
+    <div class="col-lg-6 portfolio-item">
+        <div class="card h-100">
+            <div class="card-body">
+                <form action="/orders" method="POST" role="form" enctype="multipart/form-data" class="right" >
+                    @csrf
+                    @method('POST')
 
-        <div class="form-group">
-            <label for="book_id">書籍編號：</label>
-            <input readonly="readonly" id="book_id" name="book_id" class="form-control" value="{{$book->id}}">
+                    <div class="form-group width">
+                        <label for="book_id">書籍編號：</label>
+                        <input readonly="readonly" id="book_id" name="book_id" class="form-control" value="{{$book->id}}">
+                    </div>
+                    <div class="form-group width">
+                        <label for="seller_id">賣家編號：</label>
+                        <input readonly="readonly" id="seller_id" name="seller_id" class="form-control" value="{{$book->member_id}}">
+                    </div>
+                    <div class="form-group width">
+                        <label for="name">書名：</label>
+                        <input readonly="readonly" id="name" name="name" class="form-control" value="{{$book->name}}">
+                    </div>
+                    <div class="form-group width">
+                        <label for="quantity">數量：</label>
+                        <input id="quantity" name="quantity" class="form-control" value="{{$book->quantity}}">
+                    </div>
+                    <div class="form-group width">
+                        <label for="money">價格：</label>
+                        <input readonly="readonly" id="money" name="money" class="form-control" value="{{$book->price}}">
+                    </div>
+                    <div class="form-group width">
+                        <label for="info">書籍介紹：</label>
+                        <textarea readonly="readonly" id="info" name="info" class="form-control">{{$book->info}}</textarea>
+                    </div>
+                    <button type="submit" class="btn-sm btn-primary">下單</button>
+
+                </form>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="seller_id">賣家編號：</label>
-            <input readonly="readonly" id="seller_id" name="seller_id" class="form-control" value="{{$book->member_id}}">
+    </div>
+    <div class="col-lg-3 portfolio-item">
+        <div class="card h-50">
+            <img class="card-img-top" src="/images/{{$book->path}}" alt="">
         </div>
-        <div class="form-group">
-            <label for="name">書名：</label>
-            <input readonly="readonly" id="name" name="name" class="form-control" value="{{$book->name}}">
-        </div>
-        <div class="form-group">
-            <label for="quantity">數量：</label>
-            <input id="quantity" name="quantity" class="form-control" value="{{$book->quantity}}">
-        </div>
-        <div class="form-group">
-            <label for="money">價格：</label>
-            <input readonly="readonly" id="money" name="money" class="form-control" value="{{$book->price}}">
-        </div>
-        <div class="form-group">
-            <label for="info">書籍介紹：</label>
-            <textarea readonly="readonly" id="info" name="info" class="form-control">{{$book->info}}</textarea>
-        </div>
-        <button type="submit" class="btn-sm btn-primary">下單</button>
-    </form>
+    </div>
 </div>
